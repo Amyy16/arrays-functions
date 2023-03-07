3b325383909a721965af41c59cc3ef275a331c90
Amyy16 <105382386+Amyy16@users.noreply.github.com>
2023-03-08 00:27:30 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// 1. What are the differences between mutating array methods and non-mutation array methods in JavaScript.
+//    Mutating methods are ones that change the object after the method has been used. 
+//    Non-mutating methods do not change the object after the method has been used.
+//    some of them mutate the array (changes the original array) whereas some of them create a new array.
+
+
+// List 5 array methods that fall under each of them.
+// Mutating Methods
+// array.pop()
+// array.push()
+// array.shift()
+// array.sort()
+// array.splice()
+// array.unshift().
+
+// Non-Mutating Methods
+// concat()
+// filter()
+// flat()
+// flatMap()
+// map()
+
+
+// 2. 
+
+
