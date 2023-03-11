bd12e267af57e4a3cdefc58451e699343bce837a
Amarachi <nwabueze2amarachi@gmail.com>
2023-03-11 20:16:33 +0100

    assignment updated

--- index.js
+++ index.js
@@ -21,6 +21,75 @@
 // map()
 
 
-// 2. 
+// No. 2
+
+let languages = ["C#", "Javascript", "Ruby", "PHP", "Python"];
+
+// 1.
+languages.push("kotlin")
+// console.log(languages);
+
+// //2.
+languages.splice(3, 0, "Java")
+// console.log(languages);
+
+// // 3.
+languages.shift()
+// console.log(languages);
+
+// // 4.
+languages.splice(0, 0, "Scala", "swift")
+// console.log(languages);
+
+// //5.
+languages.splice(3, 1, "Go", "Rust")
+// console.log(languages);
 
 
+// No. 3
+let fruit = ['apple', 'mango', 'banana'];
+function changeFruit( fruit ) {
+		fruit[2] = "orange";
+		return fruit;
+}
+
+// console.log(fruit);
+// The value of fruit is given as:  ['apple', 'mango', 'banana']
+
+
+// No. 4
+function Array(array) {
+    let maxNumber = array[0];
+   
+    for (let i = 1; i < array.length; i++) {
+       if (array[i] > maxNumber)
+           maxNumber = array[i];
+    }
+   
+    return maxNumber;
+   }
+
+let array = [24, 170, 350, 650, 1450, 200, 15]
+
+console.log("Largest in the array is " + Array(array));
+
+
+// No. 5
+
+function valTimesIndex(_value) {
+
+}
+
+// let new_array = [1, 3, 5, 7, 9]
+// console.log(valTimesIndex(new_array))
+
+
+function valTimesIndex(array2) {
+    return array2.map(function(value, index) {
+      return value * index;
+    });
+}
+
+let input = [2, 4, 6, 8, 10];
+let output = valTimesIndex(input);
+console.log(output); 
